cd9264d9ac71eaa27d78e3c517b0d20cd86d0617
AlessioNovi <alessionovi@yahoo.com>
2024-04-26 00:55:04 +0200

    Added component for request details body payload

--- frontend/src/components/RequestDetails.jsx
+++ frontend/src/components/RequestDetails.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import HeadersAndQuery from './HeadersAndQuery';
 import SharedDetails from './SharedDetails';
+import RequestDetailsBody from './RequestDetailsBody';
 
 const RequestDetails = ({ request }) => {
   console.log(Object.entries(request.payload));
@@ -19,12 +20,7 @@ Body component (if exists)
       <SharedDetails request={request} />
       <HeadersAndQuery type={'Headers'} obj={request.payload.headers} />
       {request.payload.query && <HeadersAndQuery type={'Query Parameters'} obj={request.payload.query} />}
-      {request.payload.body && (
-        <>
-          <b>{'Body'}</b>
-          <pre>{JSON.stringify(request.payload.body)}</pre>
-        </>
-      )}
+      {request.payload.body && <RequestDetailsBody obj={request.payload.body} />}
     </Box>
   );
 };
